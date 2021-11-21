63bb6ada4b198c6c8cad7f499143530dcb6e54ce
Toomas Roosma <toomas.roosma@gmail.com>
2021-11-21 21:48:43 +0200

    about us valmis

--- Homework3/src/components/Header.vue
+++ Homework3/src/components/Header.vue
@@ -6,7 +6,7 @@
     />
     <div>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/about">About us</router-link> |
       <router-link to="/contact">Contact</router-link>
     </div>
     <div id="account-circle" class="account-circle">
