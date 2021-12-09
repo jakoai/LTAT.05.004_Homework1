d3a2ba8631e6a1a0ca6a931939c8321dbceb3ce8
Kerdo Kurs <kerdokurs@gmail.com>
2021-12-09 10:49:49 +0200

    Set up EJS

--- Homework4/app.js
+++ Homework4/app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const { join } = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views'));
+
 app.get('/', (req, res) => {
-  res.send('ok');
+  res.render('index', {
+    val: 'ok',
+  });
 });
 
 app.listen(PORT, () => {
