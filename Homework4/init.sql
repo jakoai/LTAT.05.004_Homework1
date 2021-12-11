9be405c10363b1de1ad0634d6955a26e7ce12ccc
Toomas Roosma <toomas.roosma@gmail.com>
2021-12-11 23:29:10 +0200

    styling + footer everywhere

--- Homework4/init.sql
+++ Homework4/init.sql
@@ -20,4 +20,4 @@ INSERT INTO posts (title, body, url) VALUES ('First post', 'Have a nice day!', '
 INSERT INTO posts (title, body, url) VALUES ('Second post', 'WE ARE DOOMED!', 'https://upload.wikimedia.org/wikipedia/en/thumb/2/28/Doom_Cover.jpg/220px-Doom_Cover.jpg');
 INSERT INTO posts (title, body, url) VALUES ('Post #3', 'What tree is it?', 'https://cdn.shopify.com/s/files/1/0059/8835/2052/products/Eucalyptus_Rainbow_4_650x.jpg?v=1627506621');
 INSERT INTO posts (title, body, url) VALUES ('NEED HELP', 'I am moving out, so two cute puppies needs new home. Call 900 2255 for adoption.', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_e7AaWXpeIn8xyWSAb0zZUBgu7oRI6vWDMw&usqp=CAU');
-INSERT INTO posts (title, body, url) VALUES ('Dogs', 'Dogs are the best!', 'https://www.dogbreedinfo.com/images23c/Pug-Puppy-Dog-Pictures.jpg');
+INSERT INTO posts (title, body, url) VALUES ('Dogs', 'Dogs are the best!', 'https://thehappypuppysite.com/wp-content/uploads/2018/06/best-food-for-pug-puppies-header.jpg');
